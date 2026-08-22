822de85ae0c131bec1e3e010c0772f421ed338b0
mkwiichannel <roggerdominik59@gmail.com>
2026-08-23 00:13:43 +0200

    Create Program.cs

---
+++ MarioKartNitroRPC/Program.cs
@@ -0,0 +1,44 @@
+using DiscordRPC;
+using DiscordRPC.Logging;
+
+class Program
+{
+    // YOUR DISCORD APPLICATION ID
+    const string APPLICATION_ID = "PUT_YOUR_APPLICATION_ID_HERE";
+
+    // EXACT KEY OF YOUR DISCORD LARGE IMAGE
+    const string IMAGE_KEY = "mario_kart_nitro";
+
+    static DiscordRpcClient client;
+
+    static void Main()
+    {
+        client = new DiscordRpcClient(APPLICATION_ID);
+
+        client.Logger = new ConsoleLogger()
+        {
+            Level = LogLevel.Warning
+        };
+
+        client.Initialize();
+
+        client.SetPresence(new RichPresence()
+        {
+            Details = "Playing",
+            State = "Mario Kart Nitro",
+
+            Assets = new Assets()
+            {
+                LargeImageKey = IMAGE_KEY,
+                LargeImageText = "Mario Kart Nitro"
+            }
+        });
+
+        Console.WriteLine("Mario Kart Nitro Rich Presence is running!");
+        Console.WriteLine("Press ENTER to close.");
+
+        Console.ReadLine();
+
+        client.Dispose();
+    }
+}
